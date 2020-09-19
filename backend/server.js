ef0fbc2e1843818f2b7a486eb81b1f8c08bdc8d2
adarsh_ajit <adarshajit@gmail.com>
2020-09-19 22:56:48 +0530

    cleaned the app component

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
