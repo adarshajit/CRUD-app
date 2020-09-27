40a35176507a047aa0dd27054ec68c421b5f6ee6
adarsh_ajit <adarshajit@gmail.com>
2020-09-27 21:09:44 +0530

    tweaked the backend sleep.js route

--- backend/routes/sleep.js
+++ backend/routes/sleep.js
@@ -9,47 +9,11 @@ router.route('/').get((req,res)=>{
 
 router.route('/add').post((req,res)=>{
     const duration = Number(req.body.duration);
-    const newSleep = newSleep({duration});
+    const newSleep = new Sleep({duration});
 
     newSleep.save()
     .then(() => res.json('Sleep Duration added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
-
-
-router.route('/:id').get((req,res)=>{
-    Sleep.findById(req.params.id)
-        .then(sleep => res.json(sleep))
-        .catch(err => res.status(400).json("Error:"+ err))
-});
-
-router.route('/:id').delete((req,res)=>{
-    Sleep.findByIdAndDelete(req.params.id)
-        .then(() => res.json("Sleep Duration Deleted"))
-        .catch(err => res.status(400).json("Error:"+ err))
-});
-
-router.route('/update/:id').post((req, res) => {
-    Sleep.findById(req.params.id)
-      .then(sleep => {
- 
-        sleep.duration = Number(req.body.duration);
-    
-  
-        exercise.save()
-          .then(() => res.json('Sleep Duration updated!'))
-          .catch(err => res.status(400).json('Error: ' + err));
-      })
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
